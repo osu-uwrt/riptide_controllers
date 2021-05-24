3c6a48bbdcb413980ca4577eeb8c95229384cddb
Blaine Miller <blaine141@gmail.com>
2021-05-23 23:38:27 -0400

    Rewrite buoyancy calibration

--- action/calibrateBuoyancy.py
+++ action/calibrateBuoyancy.py
@@ -8,13 +8,12 @@ import actionlib
 import dynamic_reconfigure.client
 
 from geometry_msgs.msg import Vector3, Quaternion, Twist
-from std_msgs.msg import Float32, Empty
+from std_msgs.msg import Empty
 from nav_msgs.msg import Odometry
 import riptide_controllers.msg
 
-from tf.transformations import quaternion_from_euler, euler_from_quaternion
+from tf.transformations import quaternion_from_euler, euler_from_quaternion, quaternion_inverse, quaternion_multiply
 
-import time
 import math
 import yaml
 import numpy as np
@@ -23,9 +22,22 @@ WATER_DENSITY = 1000
 GRAVITY = 9.81
 
 def msg_to_numpy(msg):
+    """Converts a Vector3 or Quaternion message to its numpy counterpart"""
     if hasattr(msg, "w"):
         return np.array([msg.x, msg.y, msg.z, msg.w])
     return np.array([msg.x, msg.y, msg.z])
+
+def changeFrame(orientation, vector, w2b = True):
+    """Converts vector into other frame from orientation quaternion. The w2b parameter will
+     determine if the vector is converting from world to body or body to world"""
+
+    vector = np.append(vector, 0)
+    if w2b:
+        orientation = quaternion_inverse(orientation)
+    orientationInv = quaternion_inverse(orientation)
+    newVector = quaternion_multiply(orientation, quaternion_multiply(vector, orientationInv))
+    return newVector[:3]
+
 
 class CalibrateBuoyancyAction(object):
 
@@ -46,17 +58,45 @@ class CalibrateBuoyancyAction(object):
         self._as = actionlib.SimpleActionServer("calibrate_buoyancy", riptide_controllers.msg.CalibrateBuoyancyAction, execute_cb=self.execute_cb, auto_start=False)
         self._as.start()
 
+    def tune(self, initial_value, get_adjustment, apply_change, num_samples=10, delay=4):
+        """Tunes a parameter of the robot"""
+        current_value = np.array(initial_value)
+        last_adjustment = np.zeros_like(current_value)
+        converged = np.zeros_like(current_value)
+
+        while not np.all(converged):
+            # Wait for equilibrium
+            rospy.sleep(delay)
+
+            # Average a few samples
+            average_adjustment = 0
+            for _ in range(num_samples):
+                average_adjustment += get_adjustment() / num_samples
+                rospy.sleep(0.2)
+
+            # Apply change
+            current_value += average_adjustment
+            apply_change(current_value)
+
+            # Check if the value has converged
+            converged = np.logical_or(converged, average_adjustment * last_adjustment < 0)
+            last_adjustment = average_adjustment
+
+            if self.check_preempted():
+                return
+
+        return current_value
+
       
     def execute_cb(self, goal):
+        # Start reconfigure server and get starting config
         self.client = dynamic_reconfigure.client.Client("controller", timeout=30)
-
-
         self.initial_config = self.client.get_configuration()
             
-
+        # Set variables to defaults
         rospy.loginfo("Starting buoyancy calibration")
         volume = self.mass / WATER_DENSITY
-        cob = self.com
+        cob = np.copy(self.com)
 
         # Reset parameters to default
         self.client.update_configuration({
@@ -82,99 +122,75 @@ class CalibrateBuoyancyAction(object):
         odom_msg = rospy.wait_for_message("odometry/filtered", Odometry)
         current_position = msg_to_numpy(odom_msg.pose.pose.position)
         current_orientation = msg_to_numpy(odom_msg.pose.pose.orientation)
-        self.position_pub.publish(Vector3(current_position[0], current_position[1], -1.5))
+        self.position_pub.publish(Vector3(current_position[0], current_position[1], -1))
         _, _, y = euler_from_quaternion(current_orientation)
         self.orientation_pub.publish(Quaternion(*quaternion_from_euler(0, 0, y)))
 
         # Wait for equilibrium
         rospy.sleep(10)
 
-        # Do this 3 times
-        for _ in range (3):
-            # Wait for equilibrium
-            rospy.sleep(5)
+        # Volume adjustment function
+        def get_volume_adjustment():
+            body_force = self.mass * msg_to_numpy(rospy.wait_for_message("controller/requested_accel", Twist).linear)
+            orientation = msg_to_numpy(rospy.wait_for_message("odometry/filtered", Odometry).pose.pose.orientation)
+            world_z_force = changeFrame(orientation, body_force, w2b=False)[2]
 
-            # Average 10 samples
-            force_measurements = []
-            for _ in range(10):
-                force_msg = self.mass * msg_to_numpy(rospy.wait_for_message("controller/requested_accel", Twist).linear)
-                force = np.linalg.norm(force_msg)
-                if force_msg[2] < 0:
-                    force *= -1
+            return -world_z_force / WATER_DENSITY / GRAVITY
 
-                rospy.sleep(0.2)
-                force_measurements.append(force)
-
-            # Adjust volume
-            volume -= np.average(force_measurements) / WATER_DENSITY / GRAVITY
-            self.client.update_configuration({"volume": volume})
-
-            if self.check_preempted():
-                return
+        # Tune volume
+        volume = self.tune(
+            volume, 
+            get_volume_adjustment, 
+            lambda v: self.client.update_configuration({"volume": v})
+        )
 
         rospy.loginfo("Volume calibration complete")
         buoyant_force = volume * WATER_DENSITY * GRAVITY
 
-        # Adjust COB until converged
-        last_adjustment_x = 0
-        last_adjustment_y = 0
-        converged_x = False
-        converged_y = False
-        while not converged_x or not converged_y:
-            rospy.sleep(3)
-
+        # COB adjustment function
+        def get_cob_adjustment():
             accel = msg_to_numpy(rospy.wait_for_message("controller/requested_accel", Twist).angular)
             torque = self.inertia * accel
+            orientation = msg_to_numpy(rospy.wait_for_message("odometry/filtered", Odometry).pose.pose.orientation)
+            body_force_z = changeFrame(orientation, np.array([0, 0, buoyant_force]))[2]
 
-            adjustment_x = torque[1] / buoyant_force
-            adjustment_y = torque[0] / buoyant_force
-            cob[0] += adjustment_x
-            cob[1] -= adjustment_y
+            adjustment_x = torque[1] / body_force_z
+            adjustment_y = -torque[0] / body_force_z
 
-            self.client.update_configuration({"center_x": cob[0], "center_y": cob[1]})
+            return np.array([adjustment_x, adjustment_y])
 
-            # If the direction of adjustment changed, stop
-            if last_adjustment_x * adjustment_x < 0:
-                converged_x = True
-            if last_adjustment_y * adjustment_y < 0:
-                converged_y = True
-            last_adjustment_x = adjustment_x
-            last_adjustment_y = adjustment_y
-
-            if self.check_preempted():
-                return
+        # Tune X and Y COB
+        self.tune(
+            cob[:2], 
+            get_cob_adjustment, 
+            lambda cob: self.client.update_configuration({"center_x": cob[0], "center_y": cob[1]}),
+            num_samples = 2,
+            delay = 1
+        )
 
         rospy.loginfo("Buoyancy XY calibration complete")
 
         # Adjust orientation
-        current_orientation = msg_to_numpy(rospy.wait_for_message("odometry/filtered", Odometry).pose.pose.orientation)
-        _, _, y = euler_from_quaternion(current_orientation)
         self.orientation_pub.publish(Quaternion(*quaternion_from_euler(0, -math.pi / 4, y)))
-
         rospy.sleep(3)
 
-        # Adjust COB until converged
-        last_adjustment = 0
-        converged = False
-        while not converged:
-            rospy.sleep(3)
-
+        # Z COB function
+        def get_cob_z_adjustment():
             accel = msg_to_numpy(rospy.wait_for_message("controller/requested_accel", Twist).angular)
             torque = self.inertia * accel
+            orientation = msg_to_numpy(rospy.wait_for_message("odometry/filtered", Odometry).pose.pose.orientation)
+            body_force_x = changeFrame(orientation, np.array([0, 0, buoyant_force]))[0]
 
-            adjustment_z = torque[1] / buoyant_force / 2**.5
-            cob[2] -= adjustment_z
+            adjustment = -torque[1] / body_force_x
 
-            self.client.update_configuration({"center_z": cob[2]})
+            return adjustment
 
-            # If the direction of adjustment changed, stop
-            if last_adjustment * adjustment_z < 0:
-                converged = True
-            last_adjustment = adjustment_z
-
-            
-            if self.check_preempted():
-                return
+        # Tune Z COB
+        self.tune(
+            cob[2], 
+            get_cob_z_adjustment, 
+            lambda z: self.client.update_configuration({"center_z": z})
+        )
 
         rospy.loginfo("Calibration complete")
         self.cleanup()
